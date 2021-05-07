9ca70d61c141d00dc6e540f0c254bba390074bea
ryo-ma <saka_ro@yahoo.co.jp>
2021-05-08 00:59:35 +0900

    Add PersistentDrawerLeft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import PersistentDrawerLeft from '../components/PersistentDrawerLeft'
 
 function HomePage() {
-  const Map: any = dynamic(() => import('./components/Map'), { ssr: false });
-  return <Map />
+  const Map: any = dynamic(() => import('../components/Map'), { ssr: false });
+  return (
+    <div>
+      <PersistentDrawerLeft />
+      <Map />
+    </div>
+  )
 }
 
 export default HomePage
